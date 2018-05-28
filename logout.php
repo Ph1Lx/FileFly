6c4216ee436b1277e63a9a6a200a795b125b1eac
nataliestiegler <38209865+nataliestiegler@users.noreply.github.com>
2018-05-28 11:58:12 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,15 @@
-<?php
-session_start();
-session_destroy();
 
-echo "Logout erfolgreich";
+<?php
+
+session_start();
+$pdo = new PDO('mysql::host=mars.iuk.hdm-stuttgart.de.;dbname=u-ns106', 'ns106', 'se4aeda8Ai');
+
+session_unset();
+session_destroy();
+ob_start();
+header("location:landingpage.html");
+ob_end_flush();
+include 'landingpage.html';
+
+exit();
 ?>
